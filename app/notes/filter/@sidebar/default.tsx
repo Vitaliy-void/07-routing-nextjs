40e85abe565e5343ee74ce47e5d8bbee94068b94
Vitaliy Bashkir <vabashkir@icloud.com>
2025-11-10 15:48:48 +0200

    HW7 fix

---
+++ app/notes/filter/@sidebar/default.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import css from "./page.module.css";
+
+const TAGS = ["Todo", "Work", "Personal", "Meeting", "Shopping"] as const;
+
+export default function SidebarDefault() {
+  return (
+    <ul className={css.menuList}>
+      <li className={css.menuItem}>
+        <Link href="/notes/filter/all" className={css.menuLink}>
+          All notes
+        </Link>
+      </li>
+      {TAGS.map((t) => (
+        <li key={t} className={css.menuItem}>
+          <Link href={`/notes/filter/${t}`} className={css.menuLink}>
+            {t}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
